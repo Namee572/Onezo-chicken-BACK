a51cc7ee39d2c20893ba2c1aa604147e1ef4dfd1
DESKTOP-ET5K0AI\정진 <jjj200820@gmail.com>
2024-04-24 11:45:58 +0900

    검증 기능 추가

--- src/main/java/com/green/onezo/jwt/JwtTokenDto.java
+++ src/main/java/com/green/onezo/jwt/JwtTokenDto.java
@@ -1,10 +1,12 @@
 package com.green.onezo.jwt;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 
 @Getter
 @Builder
+@Data
 public class JwtTokenDto {
 
     private String accessToken;
