2bb3e289ba147bdb094d5deb0000d52a544d6434
DESKTOP-ET5K0AI\정진 <jjj200820@gmail.com>
2024-04-29 15:29:20 +0900

    어그리먼트 삭제

--- src/main/java/com/green/onezo/agreement/AgreementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.green.onezo.agreement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgreementRepository extends JpaRepository<Agreement,String> {
-
-}
