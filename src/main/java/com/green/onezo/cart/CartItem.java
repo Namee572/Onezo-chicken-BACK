f5bda9d106d08c05989c79fda64a8c4c9e6db5b8
DESKTOP-KBOS5BS\pc <myiddab@gmail.com>
2024-05-01 19:47:02 +0900

    0501 카트/회원

---
+++ src/main/java/com/green/onezo/cart/CartItem.java
@@ -0,0 +1,34 @@
+package com.green.onezo.cart;
+
+import com.green.onezo.member.Member;
+import com.green.onezo.menu.Menu;
+import com.green.onezo.store.Store;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Map;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "cart")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    @Column(nullable = false)
+    private int quantity;
+
+}
--- src/main/java/com/green/onezo/purchase/PurchaseController.java
+++ src/main/java/com/green/onezo/purchase/PurchaseController.java
@@ -1,5 +1,8 @@
 package com.green.onezo.purchase;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.*;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +15,28 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/purchase")
-@Tag(name = "purchase-controller", description = "주문 내역")
+@Tag(name = "purchase-controller", description = "메인페이지 (주문 조회/ 주문 상세 조회)")
 public class PurchaseController {
     private final PurchaseService purchaseService;
 
-
     @GetMapping("/record")
+    @Operation(summary = "주문 내역 조회", description = "주어진 주문 ID에 해당하는 주문 기록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "주문 내역 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "주어진 ID로 주문 내역을 찾을 수 없습니다"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류 발생")
+    })
     public Optional<Purchase> getRecord(@RequestParam Long id){
         return purchaseService.getPurchase(id);
     }
 
     @GetMapping("/detail")
+    @Operation(summary = "주문 상세 조회", description = "주어진 주문 상세 ID로 주문 상세 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "주문 상세 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "주어진 ID로 주문 상세를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류 발생")
+    })
     public Optional<PurchaseDetail> getDetail(@RequestParam Long id){
         return purchaseService.getDetail(id);
     }
